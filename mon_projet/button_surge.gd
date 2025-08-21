6fe9b4ed175774b543e2ea608f554b4245fbcd0a
Gllmar <gllmAR@users.noreply.github.com>
2025-08-21 15:57:52 -0400

    Add button functionality and audio integration for mallet and surge controls, update project configuration

--- mon_projet/button_surge.gd
+++ mon_projet/button_surge.gd
@@ -1,5 +1,8 @@
 extends Button
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("go_surge"):
+		self._on_pressed()
 
 func _on_pressed() -> void:
 	$"./AudioStreamPlayer-surge".play()
